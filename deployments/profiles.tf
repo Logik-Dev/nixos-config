0dc2697b25ac9bf2cc736e37b16e65ecd869fb24
Logik-Dev <logikdevfr@gmail.com>
2025-05-30 07:39:29 +0200

    deploy backup-folders profile

--- deployments/profiles.tf
+++ deployments/profiles.tf
@@ -1,6 +1,6 @@
 resource "incus_profile" "default" {
   description = "Default Incus profile"
-  name = "default"
+  name        = "default"
 
   device {
     name = "root"
@@ -18,6 +18,21 @@ resource "incus_profile" "default" {
       nictype = "macvlan"
       parent  = "enp4s0"
       vlan    = 11
+    }
+  }
+}
+
+resource "incus_profile" "backup-folders" {
+  description = "Shared backup folders"
+  name        = "backup-folders"
+
+  device {
+    name = "borg"
+    type = "disk"
+    properties = {
+      shift  = true
+      source = "/mnt/storage/borg"
+      path   = format("/home/%s/borg", nonsensitive(data.sops_file.nix_globals.data["username"]))
     }
   }
 }
