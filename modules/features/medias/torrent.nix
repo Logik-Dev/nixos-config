af47a51346008b9b339a1c79923249c6488b5f7a
Cédric Maunier <logikdevfr@gmail.com>
2026-01-04 00:33:37 +0100

    feat(medias): add qbittorrent

---
+++ modules/features/medias/torrent.nix
@@ -0,0 +1,45 @@
+{ ... }:
+{
+  flake.modules.nixos.hyper =
+    { hostSecret, config, ... }:
+    {
+
+      age.secrets."gluetun.env".rekeyFile = hostSecret "gluetun.env";
+
+      services.reverse-proxy.vhosts.torrent.port = 8080;
+
+      systemd.tmpfiles.rules = [ "d /mnt/ultra/qbittorrent 775 logikdev media - -" ];
+
+      virtualisation.oci-containers.containers = {
+        gluetun = {
+          image = "qmcgaw/gluetun";
+
+          ports = [ "8080:8080" ];
+
+          extraOptions = [
+            "--cap-add=NET_ADMIN"
+            "--device=/dev/net/tun:/dev/net/tun"
+          ];
+
+          environmentFiles = [ config.age.secrets."gluetun.env".path ];
+        };
+
+        qbittorrent = {
+          image = "lscr.io/linuxserver/qbittorrent:latest";
+          extraOptions = [ "--network=container:gluetun" ];
+          dependsOn = [ "gluetun" ];
+          volumes = [
+            "/mnt/ultra/qbittorrent:/config"
+            "/mnt/storage/medias/downloads:/mnt/storage/downloads"
+          ];
+          environment = {
+            PUID = "1000";
+            PGID = "991";
+            TZ = "Europe/Paris";
+            WEBUI_PORT = "8080";
+          };
+        };
+      };
+    };
+
+}
