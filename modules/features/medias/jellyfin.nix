42a64487c05e42c10e1341dee42fbee0446f6365
Cédric Maunier <logikdevfr@gmail.com>
2026-01-10 09:04:12 +0100

    feat(medias): jellyfin drivers

--- modules/features/medias/jellyfin.nix
+++ modules/features/medias/jellyfin.nix
@@ -1,11 +1,33 @@
 {
-  flake.modules.nixos.seedbox = {
-    services.mytraefik.services.jellyfin.port = 8096;
+  flake.modules.nixos.seedbox =
+    {
+      pkgs,
+      ...
+    }:
+    {
+      services.mytraefik.services.jellyfin.port = 8096;
+      users.users.jellyfin.extraGroups = [
+        "video"
+        "render"
+      ];
 
-    services.jellyfin = {
-      enable = true;
-      group = "media";
-      dataDir = "/mnt/ultra/jellyfin";
+      services.jellyfin = {
+        enable = true;
+        group = "media";
+        dataDir = "/mnt/ultra/jellyfin";
+      };
+
+      hardware.graphics = {
+        extraPackages = with pkgs; [
+          libva-vdpau-driver
+          nvidia-vaapi-driver
+        ];
+      };
+
+      systemd.services.jellyfin = {
+        environment = {
+          LD_LIBRARY_PATH = "/run/opengl-driver/lib";
+        };
+      };
     };
-  };
 }
