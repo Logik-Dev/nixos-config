bc62eed977330fc54103c8982c36b9fea426d0cc
Cédric Maunier <logikdevfr@gmail.com>
2026-02-13 00:23:27 +0100

    feat(storage): fix snapraid volumes

--- modules/features/storage/snapraid.nix
+++ modules/features/storage/snapraid.nix
@@ -10,17 +10,20 @@ let
 
     services.snapraid = {
       enable = true;
+      sync.interval = "10:30";
       dataDisks = {
-        storage = "/mnt/storage";
+        data1 = "/mnt/medias1";
+        data2 = "/mnt/medias2";
       };
       parityFiles = [
         "/mnt/parity1/snapraid.parity"
         "/mnt/parity2/snapraid.parity"
       ];
       contentFiles = [
-        "/mnt/ultra/.snapraid.content"
         "/mnt/local/.snapraid.content"
-        "/mnt/storage/.snapraid.content"
+        "/mnt/ultra/.snapraid.content"
+        "/mnt/medias1/.snapraid.content"
+        "/mnt/medias2/.snapraid.content"
       ];
       exclude = [
         "/lost+found/"
