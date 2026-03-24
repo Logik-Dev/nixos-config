d4ba4a2008ed746f8a694c65727361360d38ce5e
Cédric Maunier <logikdevfr@gmail.com>
2026-03-24 19:12:10 +0100

    feat(medias): fix sabnzbd config

--- modules/features/medias/sabnzbd.nix
+++ modules/features/medias/sabnzbd.nix
@@ -3,17 +3,37 @@
   flake.modules.nixos.sabnzbd =
     { config, ... }:
     {
-      traefik.services.sabnzbd.port = 8082;
+      traefik.services.sabnzbd.port = 8088;
+
+      age.secrets."sabnzbd-credentials.ini" = {
+        group = "media";
+        mode = "0440";
+      };
+
       services.sabnzbd = {
         enable = true;
+        group = "media";
+        configFile = null;
+        stateDir = "sabnzbd";
+        secretFiles = [ config.age.secrets."sabnzbd-credentials.ini".path ];
         settings = {
-          misc.port = 8082;
-          secretFiles = [ config.age.secrets."sabnzbd-credentials.ini".path ];
+          misc = {
+            port = 8088;
+            host_whitelist = "sabnzbd.hyper.logikdev.fr";
+            download_dir = "/mnt/storage/medias/downloads/incomplete";
+            complete_dir = "/mnt/storage/medias/downloads";
+            permissions = "770";
+          };
+          categories = {
+            movies.dir = "movies";
+            tv.dir = "series";
+          };
           servers.eweka = {
             port = 563;
             name = "eweka";
             host = "news.eweka.nl";
-            connections = 50;
+            connections = 20;
+            displayname = "eweka";
           };
         };
       };
