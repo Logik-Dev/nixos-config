ef0919896639cfc1f80eecb4348872c6d553a783
Cédric Maunier <logikdevfr@gmail.com>
2026-06-29 23:26:38 +0200

    fix(agenix): use absolute path for age identity

--- modules/features/security/agenix.nix
+++ modules/features/security/agenix.nix
@@ -43,7 +43,7 @@ let
           yubikeyIdentity
           {
             pubkey = "age12cd6wdnk3su0wkedg7lm5uvcg79hqw28yp6e3h6y944wcaj94cgs53w43h";
-            identity = "${config.constants.users.logikdev.homeDir}/.config/age/keys.txt";
+            identity = "/Users/logikdev/.config/age/keys.txt";
           }
         ];
 
