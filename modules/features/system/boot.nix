d3b27380de3da68a93ff178d0a6f4b989b41ef47
Cédric Maunier <logikdevfr@gmail.com>
2026-06-29 23:27:33 +0200

    feat(boot): enable memtest86 in systemd-boot menu

--- modules/features/system/boot.nix
+++ modules/features/system/boot.nix
@@ -3,6 +3,7 @@
     boot.loader.systemd-boot = {
       enable = true;
       configurationLimit = 5;
+      memtest86.enable = true;
     };
     boot.loader.efi.canTouchEfiVariables = true;
     hardware.enableRedistributableFirmware = true;
